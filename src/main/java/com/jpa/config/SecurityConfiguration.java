fa337d09bebf046d5e430ae874c2b2171bf145de
Anand Khot <anandkhot.pccoe@gmail.com>
2018-03-07 15:38:45 +0530

    Task:LDAP integration On branch LDAP Changes to be committed: 	modified:   pom.xml 	modified:   src/main/java/com/jpa/config/SecurityConfiguration.java 	modified:   src/main/resources/application.properties 	new file:   src/main/resources/test-server.ldif

--- src/main/java/com/jpa/config/SecurityConfiguration.java
+++ src/main/java/com/jpa/config/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package com.jpa.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+import java.util.Arrays;
 
 
 @EnableWebSecurity
@@ -14,9 +19,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("anand").password("anand123").roles("USER")
-		.and().withUser("prachi").password("anand123").roles("ADMIN");;
+		auth.
+				ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource(contextSource())
+				.passwordCompare()
+				.passwordEncoder(new LdapShaPasswordEncoder())
+				.passwordAttribute("userPassword");
 	}
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception
@@ -26,7 +36,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			//.antMatchers("/hello")
 			.anyRequest()
 			.fullyAuthenticated()
-			.and().httpBasic();
+				.and()
+				.formLogin().permitAll()
+			;
 		httpSecurity.csrf().disable();
+	}
+
+	@Bean
+	public DefaultSpringSecurityContextSource contextSource()
+	{
+		return new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389"),"dc=springframework,dc=org");
 	}
 }
